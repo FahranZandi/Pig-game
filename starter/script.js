de624524276e520e1881f6c5814065d6a63b53ca
FahranZandi <gmscfahranzandy@gmail.com>
2024-11-11 00:46:18 +0800

    upload

--- starter/script.js
+++ starter/script.js
@@ -1,70 +1,71 @@
-let scores, currentScore, activePlayer, playing;
+let skorPemain, skorSaatIni, pemainAktif, permainanBerjalan;
 
-const init = () => {
-  scores = [0, 0];
-  currentScore = 0;
-  activePlayer = 0;
-  playing = true;
+const mulaiPermainan = () => {
+  skorPemain = [0, 0];
+  skorSaatIni = 0;
+  pemainAktif = 0;
+  permainanBerjalan = true;
 
-  document.getElementById("score-0");
-  document.getElementById("score-1");
-  document.getElementById("current-0");
-  document.getElementById("current-1");
+  document.getElementById("score-0").textContent = '0';
+  document.getElementById("score-1").textContent = '0';
+  document.getElementById("current-0").textContent = '0';
+  document.getElementById("current-1").textContent = '0';
   document.querySelector(".dice").classList.add("hidden");
 
   document.querySelector("#section-0").classList.add("player-active");
   document.querySelector("#section-1").classList.remove("player-active");
-
+  document.querySelector("#section-0").classList.remove("player-winner");
+  document.querySelector("#section-1").classList.remove("player-winner");
   document.querySelector(".giliran-main").value = "Giliran: Pemain 1";
 };
 
-const switchPlayer = () => {
-  document.getElementById(`current-${activePlayer}`).textContent = '0';
-  currentScore = 0;
-  activePlayer = activePlayer === 0 ? 1 : 0;
+const gantiPemain = () => {
+  document.getElementById(`current-${pemainAktif}`).textContent = '0';
+  skorSaatIni = 0;
+  pemainAktif = pemainAktif === 0 ? 1 : 0;
 
   document.getElementById("section-0").classList.toggle("player-active");
   document.getElementById("section-1").classList.toggle("player-active");
 
-  document.querySelector(".giliran-main").value = `Giliran: Pemain ${activePlayer + 1}`;
+  document.querySelector(".giliran-main").value = `Giliran: Pemain ${pemainAktif + 1}`;
 };
 
 document.getElementById("btn-putar").addEventListener("click", () => {
-  if (playing) {
-    const dice = Math.trunc(Math.random() * 6) + 1;
+  if (permainanBerjalan) {
+    const angkaDadu = Math.trunc(Math.random() * 6) + 1;
 
-    const diceEl = document.querySelector(".dice");
-    diceEl.classList.remove("hidden");
-    diceEl.src = `./images/dadu-${dice}.png`;
+    const elemenDadu = document.querySelector(".dice");
+    elemenDadu.classList.remove("hidden");
+    elemenDadu.src = `./images/dadu-${angkaDadu}.png`;
 
-    if (dice !== 1) {
-      currentScore += dice;
-      document.getElementById(`current-${activePlayer}`).textContent = currentScore;
+    if (angkaDadu !== 1) {
+      skorSaatIni += angkaDadu;
+      document.getElementById(`current-${pemainAktif}`).textContent = skorSaatIni;
     } else {
-      currentScore = 0;
-      document.getElementById(`current-${activePlayer}`).textContent = currentScore;
-      switchPlayer();
+      skorSaatIni = 0;
+      document.getElementById(`current-${pemainAktif}`).textContent = skorSaatIni;
+      gantiPemain();
     }
   }
 });
 
 document.getElementById("btn-tahan").addEventListener("click", () => {
-  if (playing) {
-    scores[activePlayer] += currentScore;
-    document.getElementById(`score-${activePlayer}`).textContent = scores[activePlayer];
+  if (permainanBerjalan) {
+    skorPemain[pemainAktif] += skorSaatIni;
+    document.getElementById(`score-${pemainAktif}`).textContent = skorPemain[pemainAktif];
 
-    if (scores[activePlayer] >= 100) {
-      playing = false;
-      document.querySelector(".giliran-main").value = `Pemain ${activePlayer + 1} Menang!`;
+    if (skorPemain[pemainAktif] >= 100) {
+      permainanBerjalan = false;
+      document.querySelector(".giliran-main").value = `Pemain ${pemainAktif + 1} Menang!`;
       document.querySelector(".dice").classList.add("hidden");
+      document.getElementById(`section-${pemainAktif}`).classList.add("player-winner");
     } else {
-      currentScore = 0;
-      document.getElementById(`current-${activePlayer}`).textContent = currentScore;
-      switchPlayer();
+      skorSaatIni = 0;
+      document.getElementById(`current-${pemainAktif}`).textContent = skorSaatIni;
+      gantiPemain();
     }
   }
 });
 
-document.getElementById("btn-baru").addEventListener("click", init);
-
-init();
+document.getElementById("btn-baru").addEventListener("click", mulaiPermainan);
+mulaiPermainan();
