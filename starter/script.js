178a33499f6babcf5711de1c221d4a9c6dcee747
FahranZandi <gmscfahranzandy@gmail.com>
2024-11-25 09:34:11 +0800

    upload

--- starter/script.js
+++ starter/script.js
@@ -1,71 +1,67 @@
-let skorPemain, skorSaatIni, pemainAktif, permainanBerjalan;
+let scores, currentScore, activePlayer, playing;
 
-const mulaiPermainan = () => {
-  skorPemain = [0, 0];
-  skorSaatIni = 0;
-  pemainAktif = 0;
-  permainanBerjalan = true;
+const init = () => {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0; 
+    playing = true; 
 
-  document.getElementById("score-0").textContent = '0';
-  document.getElementById("score-1").textContent = '0';
-  document.getElementById("current-0").textContent = '0';
-  document.getElementById("current-1").textContent = '0';
-  document.querySelector(".dice").classList.add("hidden");
+    document.getElementById("score-0").textContent = 0;
+    document.getElementById("score-1").textContent = 0;
+    document.getElementById("current-0").textContent = 0;
+    document.getElementById("current-1").textContent = 0;
+    document.querySelector(".dice").classList.add("hidden"); 
 
-  document.querySelector("#section-0").classList.add("player-active");
-  document.querySelector("#section-1").classList.remove("player-active");
-  document.querySelector("#section-0").classList.remove("player-winner");
-  document.querySelector("#section-1").classList.remove("player-winner");
-  document.querySelector(".giliran-main").value = "Giliran: Pemain 1";
+    document.getElementById("section-0").classList.remove("player-winner", "player-active");
+    document.getElementById("section-1").classList.remove("player-winner", "player-active");
+    document.getElementById("section-0").classList.add("player-active"); 
+
+    document.querySelector(".giliran-main").value = "Giliran: Pemain 1"; 
 };
 
-const gantiPemain = () => {
-  document.getElementById(`current-${pemainAktif}`).textContent = '0';
-  skorSaatIni = 0;
-  pemainAktif = pemainAktif === 0 ? 1 : 0;
+const switchPlayer = () => {
+    document.getElementById(`current-${activePlayer}`).textContent = "0"; 
+    currentScore = 0; 
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    document.getElementById("section-0").classList.toggle("player-active");
+    document.getElementById("section-1").classList.toggle("player-active");
 
-  document.getElementById("section-0").classList.toggle("player-active");
-  document.getElementById("section-1").classList.toggle("player-active");
-
-  document.querySelector(".giliran-main").value = `Giliran: Pemain ${pemainAktif + 1}`;
+    document.querySelector(".giliran-main").value = `Giliran: Pemain ${activePlayer + 1}`; 
 };
 
 document.getElementById("btn-putar").addEventListener("click", () => {
-  if (permainanBerjalan) {
-    const angkaDadu = Math.trunc(Math.random() * 6) + 1;
+    if (playing) {
+        const dice = Math.trunc(Math.random() * 6) + 1;
 
-    const elemenDadu = document.querySelector(".dice");
-    elemenDadu.classList.remove("hidden");
-    elemenDadu.src = `./images/dadu-${angkaDadu}.png`;
+        const diceEl = document.querySelector(".dice");
+        diceEl.classList.remove("hidden");
+        diceEl.src = `./images/dadu-${dice}.png`;
 
-    if (angkaDadu !== 1) {
-      skorSaatIni += angkaDadu;
-      document.getElementById(`current-${pemainAktif}`).textContent = skorSaatIni;
-    } else {
-      skorSaatIni = 0;
-      document.getElementById(`current-${pemainAktif}`).textContent = skorSaatIni;
-      gantiPemain();
+        if (dice !== 1) {
+    
+            currentScore += dice;
+            document.getElementById(`current-${activePlayer}`).textContent = currentScore;
+        } else {
+            switchPlayer();
+        }
     }
-  }
 });
 
 document.getElementById("btn-tahan").addEventListener("click", () => {
-  if (permainanBerjalan) {
-    skorPemain[pemainAktif] += skorSaatIni;
-    document.getElementById(`score-${pemainAktif}`).textContent = skorPemain[pemainAktif];
+    if (playing) {
+        scores[activePlayer] += currentScore;
+        document.getElementById(`score-${activePlayer}`).textContent = scores[activePlayer];
 
-    if (skorPemain[pemainAktif] >= 100) {
-      permainanBerjalan = false;
-      document.querySelector(".giliran-main").value = `Pemain ${pemainAktif + 1} Menang!`;
-      document.querySelector(".dice").classList.add("hidden");
-      document.getElementById(`section-${pemainAktif}`).classList.add("player-winner");
-    } else {
-      skorSaatIni = 0;
-      document.getElementById(`current-${pemainAktif}`).textContent = skorSaatIni;
-      gantiPemain();
+        if (scores[activePlayer] >= 100) {
+            playing = false;
+            document.querySelector(".giliran-main").value = `Pemain ${activePlayer + 1} Menang!`;
+            document.getElementById(`section-${activePlayer}`).classList.add("player-winner");
+        } else {
+            switchPlayer();
+        }
     }
-  }
 });
 
-document.getElementById("btn-baru").addEventListener("click", mulaiPermainan);
-mulaiPermainan();
+document.getElementById("btn-baru").addEventListener("click", init);
+
+init();
